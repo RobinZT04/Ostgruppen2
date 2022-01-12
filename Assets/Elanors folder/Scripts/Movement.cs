4d16049519a68a148eb6d5a05a8eebb67799b0f2
Elanor <elanor.berg@elev.ga.lbs.se>
2022-01-12 15:54:14 +0100

    Movement på player

--- Assets/Elanors folder/Scripts/Movement.cs
+++ Assets/Elanors folder/Scripts/Movement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public Rigidbody2D body;
+    public float speed = 50;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float vert = Input.GetAxis("Vertical");
+        float horiz = Input.GetAxis("Horizontal");
+
+        body.velocity = new Vector2(horiz * speed * Time.deltaTime, vert * speed * Time.deltaTime);
     }
+
 }
