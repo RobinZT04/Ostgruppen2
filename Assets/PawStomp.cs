39b43663af39fc6f62f41aebd94079cc91da5d52
GoodGuyDeg <84770598+RobinZT04@users.noreply.github.com>
2022-01-13 12:46:42 +0100

    KATTFAN

---
+++ Assets/PawStomp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PawStomp : MonoBehaviour
+{
+    public Transform indicator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(indicator.transform.position.x, indicator.transform.position.y + 3);
+    }
+}
