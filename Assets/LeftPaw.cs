d037f03ad30fbbdb3217084d2ab4dce6b1067fd6
GoodGuyDeg <84770598+RobinZT04@users.noreply.github.com>
2022-01-13 14:30:50 +0100

    Attack :DDDD
    Left Paw Attack :D

--- Assets/LeftPaw.cs
+++ Assets/LeftPaw.cs
@@ -4,30 +4,47 @@ using UnityEngine;
 
 public class LeftPaw : MonoBehaviour
 {
+    public Transform Paw; //kordinaterna till indicatorn
     public Transform targetPlayer;
     public float speed;
     public float distance;
 
-    public bool following;
+    public static bool following;
+    public static bool attackpaw;
+
+    public static bool Canattack;
+
+    public Animator bonkanim;
 
     public void Start()
     {
         following = true;
-        StartCoroutine(Attackera());
     }
     // Update is called once per frame
     void Update()
     {
-        if (following)
+        if (following) //if following
         {
-            if (Vector2.Distance(transform.position, targetPlayer.position) >= distance)
+            if (Vector2.Distance(transform.position, targetPlayer.position) >= distance) //om positionen är högre än tillåtna distansen
             {
-                transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, speed * Time.deltaTime);
+                Paw.transform.position = new Vector2(transform.position.x, transform.position.y + 3);
+                transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, speed * Time.deltaTime); //rör transform mot player positionen
             }
         }
+        if (Vector2.Distance(transform.position, targetPlayer.position) <= 0.3) //om positionen är högre än tillåtna distansen
+        {
+            following = false;
+            StartCoroutine(Attacking());
+        }
     }
-    IEnumerator Attackera()
+
+    IEnumerator Attacking() //coroutine Attacking
     {
-        yield return new WaitForSeconds(Random.Range(1, 5));
+        bonkanim.SetBool("Bonk", true);
+        //Paw.transform.position = new Vector2(transform.position.x, transform.position.y - 0.45f);
+        yield return new WaitForSeconds(2); //väntar i 2 sekunder
+        bonkanim.SetBool("Bonk", false);
+        following = true;
     }
+
 }
--- Assets/PawStomp.cs
+++ Assets/PawStomp.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class PawStomp : MonoBehaviour
 {
-    public Transform indicator;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform indicator; //kordinaterna till indicatorn
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector2(indicator.transform.position.x, indicator.transform.position.y + 3);
+        transform.position = new Vector2(indicator.transform.position.x, indicator.transform.position.y + 3); //sätt handen till indikatorns position + 3;
+    }
+
+    IEnumerator Attacking() //coroutine Attacking
+    {
+        transform.position = new Vector2(indicator.transform.position.x, indicator.transform.position.y - 0.25f);
+        yield return new WaitForSeconds(2); //väntar i 2 sekunder
     }
 }
