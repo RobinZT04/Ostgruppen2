0abf883ffc3af678e6c9c43037bc9e3b0a3c5247
Lidok123 <79506296+Lidok123@users.noreply.github.com>
2022-01-13 16:14:08 +0100

    Knapp pussel kod

---
+++ Assets/Henrys Folder/DoorButtons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButtons : MonoBehaviour
+{
+    public Light buttonlight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        buttonlight.enabled = true;
+        /*if (true)
+        {
+
+        }*/
+    }
+}
