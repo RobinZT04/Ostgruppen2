c1bd271553d2d68e80ee0c1a176e914fc45251f6
GoodGuyDeg <84770598+RobinZT04@users.noreply.github.com>
2022-01-14 14:08:26 +0100

    beep boop

--- Assets/Boss.cs
+++ Assets/Boss.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Boss : MonoBehaviour
-{
-    public static float Health;
-    public Slider healthbar;
-    public bool up;
+{ 
+    public static float Health; //float health
+    public Slider healthbar; //referens till healthbar
+    public bool up; //bool up
     
     // Start is called before the first frame update
     void Start()
     {
 
-        Health = 100;
-        healthbar.maxValue = 100;
+        Health = 100; //health är 100
+        healthbar.maxValue = 100; //maxvalue är 100
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthbar.value = Health;
-        if(Health <= 50)
+        healthbar.value = Health; //value är health
+        if(Health <= 50) // om health är under 50
         {
-            LeftPaw.speed = 3.5f;
-        } else
+            LeftPaw.speed = 3.5f; //sätt speed till 3.5f
+        } else //annars
         {
-            LeftPaw.speed = 2;
+            LeftPaw.speed = 2; //sätt speed till 2
         }
     }
 }
