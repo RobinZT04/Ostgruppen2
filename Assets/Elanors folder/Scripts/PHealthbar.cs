f85baba2ee50aeda453e16c8fad9981b8790f599
Elanor <elanor.berg@elev.ga.lbs.se>
2022-01-14 14:18:01 +0100

    player kan ta damage och kommenterat kod
    player tar just nu 2 damage om katten nuddar än

--- Assets/Elanors folder/Scripts/PHealthbar.cs
+++ Assets/Elanors folder/Scripts/PHealthbar.cs
@@ -5,20 +5,20 @@ using UnityEngine.UI;
 
 public class PHealthbar : MonoBehaviour
 {
-    public Slider healthbar;
-    public float health;
+    public Slider healthbar; //En referens till min slider- Elanor
+    public static float health; //Ean float som heter health- Elanor
 
     // Start is called before the first frame update
     void Start()
     {
-        health = 10;
-        healthbar.maxValue = 10;
+        health = 10; //player har 10 health när spelets startas- Elanor
+        healthbar.maxValue = 10; //Max health är 10 hp- Elanor 
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthbar.value = health;
+        healthbar.value = health; //helathbars value är samma sak som health- Elanor
 
     }
 }
