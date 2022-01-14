d1587f5e0121b640bdf680877eb8bd958772be3e
Elanor <elanor.berg@elev.ga.lbs.se>
2022-01-14 16:15:42 +0100

    Restart  button musen kan även dö :)

--- Assets/Elanors folder/Scripts/Movement.cs
+++ Assets/Elanors folder/Scripts/Movement.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public Rigidbody2D body; // En rigedbody som heter body- Elanor
-    public float speed = 50; // En float som heter speed, speeden är nu 50 men kan ändras i unity eller här- Elanor
+    public static float speed = 1000; // En float som heter speed, speeden är nu 50 men kan ändras i unity eller här- Elanor
+    public AudioSource asfotsteg; // Refrens till min audiosorce- Elanor 
+    public AudioClip acsteg; //Referens till players audioclip- Elanor
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -20,7 +22,9 @@ public class Movement : MonoBehaviour
         float horiz = Input.GetAxis("Horizontal"); // sätter float horiz värdet till Input.GetAxis("Horizontal")- Elanor
 
         body.velocity = new Vector2(horiz * speed * Time.deltaTime, vert * speed * Time.deltaTime); // Gör så att spelarn kan röra sig - Elanor
-      
+
+        asfotsteg.pitch = Random.Range(-0.5f, 2); //Audiosorces pitch kan ändras mellan -0.5 och 2- Elanor
+
         if (Input.GetKeyDown(KeyCode.A)) //Om man trycker ner A?
         {
             transform.localEulerAngles = new Vector2(0, 180); //Så kommer Spriten spegelvändas- Elanor
@@ -30,6 +34,13 @@ public class Movement : MonoBehaviour
             transform.localEulerAngles = new Vector2(0, 0);//Så kommer Spriten vändas tillbaka till höger- Elanor
         }
 
-    }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.W)) //Om man trycker på D eller A eller S eller W?
+        {
+            if (!asfotsteg.isPlaying) //Om audiosorces inte spelas?
+            {
+                asfotsteg.PlayOneShot(acsteg, 1); //spela Audioclip med volymen 1- Elanor
+            }
 
+        }
+    }
 }
--- Assets/Elanors folder/Scripts/PHealthbar.cs
+++ Assets/Elanors folder/Scripts/PHealthbar.cs
@@ -2,23 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PHealthbar : MonoBehaviour
 {
     public Slider healthbar; //En referens till min slider- Elanor
     public static float health; //Ean float som heter health- Elanor
+    public GameObject restart; //referens till gamgeobject restart- Elanor 
 
     // Start is called before the first frame update
     void Start()
     {
-        health = 10; //player har 10 health när spelets startas- Elanor
+        health = 0; //player har 10 health när spelets startas- Elanor
         healthbar.maxValue = 10; //Max health är 10 hp- Elanor 
+        restart.SetActive(false); //restart objectet är inaktiv när spelet börjar- Elanor 
     }
 
     // Update is called once per frame
     void Update()
     {
         healthbar.value = health; //helathbars value är samma sak som health- Elanor
+        
+        if(health <= 0) //om health är mindre eller är 0?
+        {
+            Movement.speed = -0; //så ska speed bli 0- Elanor
+            restart.SetActive(true); //Och restart ska aktiveras- Elanor 
+        }
 
     }
+
+    public void Restartgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //laddar den nuvarande scenen igen- Elanor
+    }
+
 }
