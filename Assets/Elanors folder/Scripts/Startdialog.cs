fa8ab36ad157a8438168124d4dc337ede77e3e3c
Elanor <elanor.berg@elev.ga.lbs.se>
2022-01-18 12:24:03 +0100

    Komenterat kod

--- Assets/Elanors folder/Scripts/Startdialog.cs
+++ Assets/Elanors folder/Scripts/Startdialog.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 
 public class Startdialog : MonoBehaviour
 {
-    public int intruduktion = 4;
-    public GameObject tankebubblan;
-    public Text dialog;
+    public int intruduktion = 4; // En int som har värdet 4 - Elanor 
+    public GameObject tankebubblan; //Refrens till ett gameobject- Elanor 
+    public Text dialog; //En referens till min text - Elanor 
 
     // Start is called before the first frame update
     void Start()
@@ -18,29 +18,29 @@ public class Startdialog : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)) //Om man trycker ner space?- Elanor 
         {
-            intruduktion -= 1;
+            intruduktion -= 1; //När man trycke space kommer intruduktion minska med 1- Elanor 
         }
-        switch (intruduktion)
+        switch (intruduktion) //En switch- Elanor 
         {
-            case 4:
-                dialog.text = "bajs";
-                Movement.speed = -0;
-                break;
-            case 3:
-                dialog.text = "Leess goo baby";
-                break;
-            case 2:
-                dialog.text = "Lee go baby";
-                break;
-            case 1:
-                dialog.text = "Les oo bb";
-                break;
-            case 0:
-                Movement.speed = 1000;
-                tankebubblan.SetActive(false);
-                break;
+            case 4: // Case 4- Elanor
+                dialog.text = "To move use W,A,S,D, press space for next thought"; //Säger till att min text ska skriva ut det jag har skrivt här - Elanor
+                Movement.speed = -0; //Min movement speed blir 0 så playern kan inte gå- Elanor
+                break; //Dialogen försvinner- Elanor 
+            case 3:// Case 3 - Elanor
+                dialog.text = "You also have a sword to use it press space , dont be afraid to use it! \n now lets see what this mouse is doing"; //Säger till att min text ska skriva ut det jag har skrivt här - Elanor
+                break;//Dialogen försvinner- Elanor 
+            case 2:// Case 2 - Elanor
+                dialog.text = "What a nice sleep, whait where am I, hello is someone there?"; //Säger till att min text ska skriva ut det jag har skrivt här - Elanor
+                break;//Dialogen försvinner- Elanor 
+            case 1: // Case 1 - Elanor
+                dialog.text = "seems like im all alone in this place, well \n lets explore I need to fin a way out!"; //Säger till att min text ska skriva ut det jag har skrivt här - Elanor
+                break;//Dialogen försvinner- Elanor 
+            case 0: // Case 0 - Elanor
+                Movement.speed = 1000; //Movement speed blir tillbaka till som vanligt- Elanor
+                tankebubblan.SetActive(false); //Tanke bubblan blir inaktiv och försvinner- Elanor 
+                break;//Dialogen försvinner- Elanor 
         }
     }
 }
