bcdd55ce4fff962760e74ba4c31d722562db1f49
GoodGuyDeg <84770598+RobinZT04@users.noreply.github.com>
2022-01-20 13:31:15 +0100

    Meny

---
+++ Assets/ButtonScriptMeny.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScriptMeny : MonoBehaviour
+{
+public void StartGame() //StartGame - Elanor och robin
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void QuitGame() //Quit game - Elanor och Robin
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+    public void Fullscreenmode() //Fullscreen - Robin
+    {
+            Screen.fullScreen = Screen.fullScreen;
+        Debug.Log("Fullscreen");
+    }
+
+}
