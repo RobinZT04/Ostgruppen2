dafbdecb4f0076f7438ae97970d1f1b980b0a361
Elanor <elanor.berg@elev.ga.lbs.se>
2022-01-20 17:08:53 +0100

    kameran kan följa spelaren nu

---
+++ Assets/Elanors folder/Scripts/Camerafollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerafollow : MonoBehaviour
+{
+    public float followspeed = 2f; //En referens till en float- Elanor
+    public Transform target; //En referens till en transform- Elanor 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f); //Den ska följa min targets position- Elanor 
+        transform.position = Vector3.Slerp(transform.position, newPos, followspeed * Time.deltaTime); // Ändrar kamerans position till targets position- Elanor 
+    }
+}
