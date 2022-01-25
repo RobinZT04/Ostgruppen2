95b0b220f4d3963990f8366ce829b6f8663f75c9
leaguecomapany <74959602+leaguecomapany@users.noreply.github.com>
2022-01-25 20:26:35 +0100

    Inget
    ...

---
+++ Assets/Luis-Folder/Script/HUD.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    [Header("Reference")]
+    public Animator animator;
+
+    public TextMeshProUGUI coinsMesh;
+ 
+    [SerializeField] private Image inventoryItemGraphic;
+    [SerializeField] private GameObject startUp;
+
+    [System.NonSerialized] public Sprite blankUI; // The sprite that is shown in the UI when you don't have any items
+    private float coins;
+    private float coinsEased;
+
+
+
+    void Start()
+    {
+        //Set all bar widths to 1, and also the smooth variables.
+       
+        coins = (float)NewPlayer.Instance.coins;
+        coinsEased = coins;
+        blankUI = inventoryItemGraphic.GetComponent<Image>().sprite;
+    }
+
+    void Update()
+    {
+        //Update coins text mesh to reflect how many coins the player has! However, we want them to count up.
+        coinsMesh.text = Mathf.Round(coinsEased).ToString();
+        coinsEased += ((float)NewPlayer.Instance.coins - coinsEased) * Time.deltaTime * 5f;
+
+        if (coinsEased >= coins)
+        {
+            animator.SetTrigger("getGem");
+            coins = coinsEased + 1;
+        }
+
+       
+
+    }
+
+    public void HealthBarHurt()
+    {
+        animator.SetTrigger("hurt");
+    }
+
+    public void SetInventoryImage(Sprite image)
+    {
+        inventoryItemGraphic.sprite = image;
+    }
+
+    
+}
---
+++ Assets/Scenes/NewPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NewPlayer : PhysicsObject
+{
+
+    // Singleton instantiation
+    private static NewPlayer instance;
+    public static NewPlayer Instance
+    {
+        get
+        {
+            if (instance == null) instance = GameObject.FindObjectOfType<NewPlayer>();
+            return instance;
+        }
+    }
+
+
+    [System.NonSerialized] public string groundType = "grass";
+    [System.NonSerialized] public RaycastHit2D ground;
+
+    [Header("Inventory")]
+    public int coins;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
