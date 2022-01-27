f409cdcb2b1861db91ddc5a6dda2644d722376c8
GoodGuyDeg <84770598+RobinZT04@users.noreply.github.com>
2022-01-27 15:23:57 +0100

    MENU

--- Assets/ButtonScriptMeny.cs
+++ Assets/ButtonScriptMeny.cs
@@ -5,7 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class ButtonScriptMeny : MonoBehaviour
 {
-public void StartGame() //StartGame - Elanor och robin
+    public bool active;
+    public Animator optiontab;
+    public bool animating;
+
+    public void Start()
+    {
+        animating = false;
+        active = false;
+    }
+    public void StartGame() //StartGame - Elanor och robin
     {
         SceneManager.LoadScene("Labyrint");
     }
@@ -16,8 +25,37 @@ public void StartGame() //StartGame - Elanor och robin
     }
     public void Fullscreenmode() //Fullscreen - Robin
     {
-            Screen.fullScreen = Screen.fullScreen;
+        Screen.fullScreen = Screen.fullScreen;
         Debug.Log("Fullscreen");
+    }
+
+    public void Options()
+    {
+        if (!animating)
+        {
+            if (!active)
+            {
+                optiontab.SetBool("Up", true);
+                optiontab.SetBool("Down", false);
+                animating = true;
+                StartCoroutine(Animating());
+                active = true;
+            }
+            else
+            {
+                optiontab.SetBool("Down", true);
+                optiontab.SetBool("Up", false);
+                animating = true;
+                StartCoroutine(Animating());
+                active = false;
+            }
+        }
+    }
+
+    IEnumerator Animating()
+    {
+        yield return new WaitForSeconds(2);
+        animating = false;
     }
 
 }
