b5af11cd67a7ba10c52ec208ed32bb29b3a1a02d
WilliamS <william.skyllberg@elev.ga.lbs.se>
2022-01-28 15:22:42 +0100

    låda

---
+++ Assets/Williams Mapp/Scripts/Låda.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Låda : MonoBehaviour
+{
+    public Sprite förstördLådaSprite;
+    SpriteRenderer spriteRend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Sword")
+        {
+            spriteRend.sprite = förstördLådaSprite;
+        }
+    }
+}
