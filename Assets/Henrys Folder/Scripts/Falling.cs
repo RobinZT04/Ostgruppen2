30d602742c18c1032620835aac81598b0eb0295a
Lidok123 <79506296+Lidok123@users.noreply.github.com>
2022-02-01 09:49:54 +0100

    Update Falling.cs

--- Assets/Henrys Folder/Scripts/Falling.cs
+++ Assets/Henrys Folder/Scripts/Falling.cs
@@ -19,7 +19,7 @@ public class Falling : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         StartCoroutine(Fall());
     }
