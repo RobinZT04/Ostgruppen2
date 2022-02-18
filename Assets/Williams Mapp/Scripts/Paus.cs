830602397cd4a69e1559927976447225609f3f25
WilliamS <william.skyllberg@elev.ga.lbs.se>
2022-02-18 14:31:05 +0100

    paus (ej klar)

---
+++ Assets/Williams Mapp/Scripts/Paus.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Paus : MonoBehaviour
+{
+    public GameObject options;
+    public bool paused;
+    // Update is called once per frame
+    void Update()
+    {
+        //Pausar spelet när man klickar på escape. -William
+        if (Input.GetKeyDown(KeyCode.Escape) && !paused)
+        {
+            options.SetActive(true);
+            Time.timeScale = 0;
+            paused = true;
+        }
+        //Ifall spelet redan är pausat så startas spelet igen när man klickar på escape. -William
+        else if (Input.GetKeyDown(KeyCode.Escape) && paused)
+        {
+            options.SetActive(false);
+            Time.timeScale = 1;
+            paused = false;
+        }
+    }
+    //Startar spelet igen. -William
+    public void Unpause()
+    {
+        options.SetActive(false);
+        Time.timeScale = 1;
+    }
+    //Ändrar scenen till menyscenen. -William
+    public void Quit()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
