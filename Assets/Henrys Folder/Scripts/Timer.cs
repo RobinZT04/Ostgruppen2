ea51ffc9cd0ae190cbd5439b4704c647dd69ca5f
Lidok123 <79506296+Lidok123@users.noreply.github.com>
2022-02-18 14:38:45 +0100

    timer grejer

--- Assets/Henrys Folder/Scripts/Timer.cs
+++ Assets/Henrys Folder/Scripts/Timer.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     public GameObject textDisplay;
+    public int timeLeft = 30;
+    public bool takingAway = false;
 
     // Start is called before the first frame update
     void Start()
